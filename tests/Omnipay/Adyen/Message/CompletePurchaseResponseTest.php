3280204cff040376832ce959225ac162957b2567
Mujib Azizi <mujib@nilvola.nl>
2015-02-24 14:27:49 +0100

    Update tests

--- tests/Omnipay/Adyen/Message/CompletePurchaseResponseTest.php
+++ tests/Omnipay/Adyen/Message/CompletePurchaseResponseTest.php
@@ -12,42 +12,70 @@ class CompletePurchaseResponseTest extends TestCase
             $this->getMockRequest(),
             array(
                 'success' => true,
+                'allParams' => [
+                    'merchantSig' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
+                ],
+                'responseSignature' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
             )
         );
-        
+
         $this->assertTrue($response->isSuccessful());
     }
-    
+
     public function testCompletePurchaseFailure()
     {
         $response = new CompletePurchaseResponse(
             $this->getMockRequest(),
             array(
                 'success' => false,
+                'allParams' => [
+                    'merchantSig' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
+                ],
+                'responseSignature' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
             )
         );
-        
+
         $this->assertFalse($response->isSuccessful());
     }
-    
+
+    public function testNonMatchingSignature()
+    {
+        $response = new CompletePurchaseResponse(
+            $this->getMockRequest(),
+            array(
+                'success' => true,
+                'allParams' => [
+                    'merchantSig' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
+                ],
+                'responseSignature' => 'Wkf3d2PVmSPu5Zn5N5H240AQfJ0='
+            )
+        );
+
+        $this->assertFalse($response->isSuccessful());
+    }
+
     public function testIsSuccessful()
     {
         $response = new CompletePurchaseResponse(
             $this->getMockRequest(),
             array(
                 'success' => true,
+                'allParams' => [
+                    'merchantSig' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
+                ],
+                'responseSignature' => 'Ti+ACycv7TmV3VY6hfQ6tIIdhGM='
             )
         );
-        
+
         $this->assertTrue($response->isSuccessful());
     }
-    
+
     public function testGetResponse()
     {
-        
+
         $mock = $this->getMockRequest();
         $response = new CompletePurchaseResponse($mock, array());
         $this->assertSame(serialize($response), serialize($response->getResponse()));
-        
+
     }
 }
